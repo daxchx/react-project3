0aed69df6891d64aa82e0c80e1ab87aee3c72a18
daxchx <daxchx.v1@gmail.com>
2023-11-06 22:11:38 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,83 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+interface ItemProps {
+  value: string | number;
+  label: string;
+  onChange: any;
+}
 
-export default function Home() {
+function Item({ value, label, onChange }: ItemProps) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <div className="pb-4">
+        <label
+          htmlFor={label}
+          className="inline-block mb-1 p-1 rounded bg-gray-900 text-xs text-white"
+        >
+          {label}
+        </label>
+        <input
+          className="w-full p-1 border rounded bg-gray-100"
+          id={label}
+          type="text"
+          value={value}
+          onChange={onChange}
         />
       </div>
+    </>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function App() {
+  const [decimal, setDecimal] = useState<string | number>("");
+  const [binary, setBinary] = useState<string | number>("");
+  const [hexadecimal, setHexadecimal] = useState<string | number>("");
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const [result, setResult] = useState("");
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+  function handleDecimal(e: any) {
+    let target = +e.target.value;
+    setDecimal(target ? target : "");
+    setBinary(target ? target.toString(2) : "");
+    setHexadecimal(target ? target.toString(16) : "");
+    setResult("10進数 - " + target.toString());
+  }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  function handleBinary(e: any) {
+    let target = e.target.value;
+    setDecimal(parseInt(target, 2) ? parseInt(target, 2) : "");
+    setBinary(parseInt(target, 2) ? target : "");
+    setHexadecimal(parseInt(target, 2) ? parseInt(target, 2).toString(16) : "");
+    setResult("2進数 - " + target);
+    console.log(parseInt("1000222", 2));
+  }
+
+  function handleHexadecimal(e: any) {
+    let target = e.target.value;
+    setDecimal(parseInt(target, 16) ? parseInt(target, 16) : "");
+    setBinary(parseInt(target, 16) ? parseInt(target, 16).toString(2) : "");
+    setHexadecimal(target != "" ? target : "");
+    setResult("16進数 - " + target);
+  }
+
+  return (
+    <div className="max-w-xs mx-auto">
+      <h1 className="mb-4 py-4 border-b text-xl font-bold text-center">
+        数字が100以上か判定
+      </h1>
+      <Item value={decimal} label="10進数" onChange={handleDecimal} />
+      <Item value={binary} label="2進数" onChange={handleBinary} />
+      <Item value={hexadecimal} label="16進数" onChange={handleHexadecimal} />
+      {+decimal >= 100 && (
+        <div className="inline-block p-1 bg-green-100 border border-green-500 rounded text-base text-green-500">
+          {result}
+        </div>
+      )}
+      {+decimal < 100 && +decimal > 0 && (
+        <div className="inline-block p-1 bg-red-100 border border-red-500 rounded text-base text-red-500">
+          {result}
+        </div>
+      )}
+    </div>
+  );
 }
